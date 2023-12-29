e7eb0677f077f94f6b1ec80c50aa7927b34fc52c
MΛKI <mo_elmaki01@hotmail.com>
2023-12-28 20:59:09 -0500

    Incorporate a camera

--- src/level.py
+++ src/level.py
@@ -3,13 +3,15 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+
+
 class Level:
     def __init__(self):
         # get the display surface
         self.display_surface = pygame.display.get_surface()
         
         # Sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         # sprite setup
@@ -27,6 +29,25 @@ class Level:
 
     def run(self):
         # Update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        
+        # General Setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2(0,0)
+
+    def custom_draw(self, player):
+
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        for sprite in self.sprites():
+            offset_position = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_position)
+    
